83031481769c9b61a03ee61c05fa8846eb81afa5
Lincoln Colling <lincoln@colling.net.nz>
2020-09-17 12:58:19 +0100

    Update helpers.R

--- R/helpers.R
+++ R/helpers.R
@@ -1,5 +1,5 @@
 #' @export
-worksheet = function(theme, highlight= "textmate", toc = FALSE) {
+worksheet = function() {
   # locations of resource files in the package
   pkg_resource = function(...) {
     system.file(..., package = "worksheets")
@@ -10,9 +10,9 @@ worksheet = function(theme, highlight= "textmate", toc = FALSE) {
 
   # call the base html_document function
   rmarkdown::html_document(
-    toc = toc, fig_width = 6.5, fig_height = 4,
-    theme = theme, css = css,
-    highlight = highlight,
+    toc = FALSE,
+    theme = "default", css = css,
+    highlight = "tango",
     includes = rmarkdown::includes(after_body = footer)
   )
 }
